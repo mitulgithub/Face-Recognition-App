de48387acdbf7a8b39d48eee515cde09b505a3c0
AnkushGrover-code <ankushgrover.0907@gmail.com>
2020-04-02 12:34:56 +0530

    adding main

--- face_classifier.py
+++ face_classifier.py
@@ -30,71 +30,77 @@ def knn(train, test, k=5):
 ################################
 
 
-cap = cv2.VideoCapture(0)
+########## MAIN ############
+def main():
+    cap = cv2.VideoCapture(0)
 
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_alt.xml')
+    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_alt.xml')
 
-skip = 0
-dataset_path = './data/'
+    skip = 0
+    dataset_path = './data/'
 
-face_data = []
-labels = []
-class_id = 0 #labels for the given file 
-names = {} #dictionary for the mapping of labels to people 
+    face_data = []
+    labels = []
+    class_id = 0 #labels for the given file 
+    names = {} #dictionary for the mapping of labels to people 
 
-#data preparation 
+    #data preparation 
 
-for fx in os.listdir(dataset_path):
+    for fx in os.listdir(dataset_path):
 
-    names[class_id] = fx[:-4]   # Mapping class label and name 
-
-
-    if fx.endswith('.npy'):
-        print("loaded: "+fx)
-        data_item = np.load(dataset_path+fx)
-        face_data.append(data_item)
-
-        target = class_id*np.ones((data_item.shape[0],))
-        class_id += 1
-        labels.append(target)
-
-    
-face_dataset = np.concatenate(face_data,axis=0)
-face_labels = np.concatenate(labels,axis=0).reshape((-1,1))
-
-trainset = np.concatenate((face_dataset,face_labels),axis = 1)
-
-# Testing
-
-while True:
-    ret,frame = cap.read()
-    if ret == False:
-        continue 
-    faces = face_cascade.detectMultiScale(frame,1.3,5)
-
-    for face in faces:
-        x,y,w,h = face
-
-        #ROI Region of interest area
-        offset = 10
-        face_section = frame[y-offset:y+h+offset,x-offset:x+w+offset]
-        face_section = cv2.resize(face_section,(100,100))
-
-        out = knn(trainset,face_section.flatten()) #Prediction for the screen
-
-        #Display the rectangle with the prediction 
-        pred_name = names[int(out)]
-
-        cv2.putText(frame,pred_name,(x,y-10),cv2.FONT_HERSHEY_SIMPLEX,1,(255,0,0),2,cv2.LINE_AA)
-        cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,255),2)
+        names[class_id] = fx[:-4]   # Mapping class label and name 
 
 
-    cv2.imshow("faces", frame)
+        if fx.endswith('.npy'):
+            print("loaded: "+fx)
+            data_item = np.load(dataset_path+fx)
+            face_data.append(data_item)
 
-    key = cv2.waitKey(1) & 0xFF
-    if key == ord('q'):
-        break
+            target = class_id*np.ones((data_item.shape[0],))
+            class_id += 1
+            labels.append(target)
 
-cap.release()
-cv2.destroyAllWindows()
+        
+    face_dataset = np.concatenate(face_data,axis=0)
+    face_labels = np.concatenate(labels,axis=0).reshape((-1,1))
 
+    trainset = np.concatenate((face_dataset,face_labels),axis = 1)
+
+    # Testing
+
+    while True:
+        ret,frame = cap.read()
+        if ret == False:
+            continue 
+        faces = face_cascade.detectMultiScale(frame,1.3,5)
+
+        for face in faces:
+            x,y,w,h = face
+
+            #ROI Region of interest area
+            offset = 10
+            face_section = frame[y-offset:y+h+offset,x-offset:x+w+offset]
+            face_section = cv2.resize(face_section,(100,100))
+
+            out = knn(trainset,face_section.flatten()) #Prediction for the screen
+
+            #Display the rectangle with the prediction 
+            pred_name = names[int(out)]
+
+            cv2.putText(frame,pred_name,(x,y-10),cv2.FONT_HERSHEY_SIMPLEX,1,(255,0,0),2,cv2.LINE_AA)
+            cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,255),2)
+
+
+        cv2.imshow("faces", frame)
+
+        key = cv2.waitKey(1) & 0xFF
+        if key == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+################################
+
+# calling the main function
+if __name__ == '__main__':
+    main();
